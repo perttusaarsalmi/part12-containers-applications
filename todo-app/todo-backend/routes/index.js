e2c574dfdd60618c567a74331851cd4108e26adc
Perttu Saarsalmi <ext.perttu.saarsalmi@metsagroup.com>
2025-10-20 15:28:27 +0300

    osa 12 steps 9-11

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,17 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { getAsync } = require("../redis");
 
-const configs = require('../util/config')
+const configs = require("../util/config");
 
-let visits = 0
+let visits = 0;
 
 /* GET index data. */
-router.get('/', async (req, res) => {
-  visits++
+router.get("/", async (req, res) => {
+  visits++;
 
   res.send({
     ...configs,
-    visits
+    visits,
+  });
+});
+
+/* GET statistics. */
+router.get("/statistics", async (req, res) => {
+  const addedTodos = (await getAsync("added_todos")) || "0";
+  res.send({
+    added_todos: parseInt(addedTodos),
   });
 });
 
